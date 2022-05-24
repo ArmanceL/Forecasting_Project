981cb025fbc746e1e8d658797288fbf330066a5b
nbidet <32996297+nbidet@users.noreply.github.com>
2022-05-24 17:17:46 +0200

    Update setup.R

--- Setup/setup.R
+++ Setup/setup.R
@@ -9,7 +9,7 @@ packages <- c(
   "ggrepel", "gghighlight", "patchwork", "maps", "scales", "tidyquant", "crosstalk", "ggpubr", "cowplot", "ggfortify",  # for plotting
   "bookdown", "rmarkdown", "knitr", "systemfonts", "kableExtra","plotly", "leaflet", "countrycode", # for the report
   "stargazer", "corpcor", "corrplot", "ppcor", "GGally", "mctest", "pls", "factoextra", # for the analysis
-  "forecast", "visdat", "vars"
+  "forecast", "visdat", "vars","padr","visdat"
 )
 
 purrr::walk(packages, library, character.only = TRUE)
